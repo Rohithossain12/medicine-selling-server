f2021117342fb889822450e0bb546ed44a6fe8cf
Rohithossain12 <https://github.com/Rohithossain12>
2025-01-28 15:39:53 +0600

    vercel file added

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin:[
+    'http://localhost:5173',
+    'https://starlit-bombolone-866a08.netlify.app/'
+  ],
+  credentials: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uv360.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -601,9 +607,9 @@ async function run() {
     });
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
